4f11e269694af6f55caac878f16ffbbf83d8452e
avbutlov <butloff666@gmail.com>
2021-03-19 14:27:07 +0300

    refactor: header to separated component

---
+++ src/cart-button/cart-button.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+
+export default class CartButton extends Component {
+
+    render() {
+        return (
+            <button>Hello</button>
+        )
+    }
+}
